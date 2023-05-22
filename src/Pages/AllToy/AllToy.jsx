c455a77d271f6781899185d55f1fb9640b1b54f3
mikabir76 <kabirmd0987@gmail.com>
2023-05-22 23:27:40 +0600

    search index created

--- src/Pages/AllToy/AllToy.jsx
+++ src/Pages/AllToy/AllToy.jsx
@@ -7,6 +7,7 @@ import { ImSearch } from "react-icons/im";
 const AllToy = () => {
     dynamicTitle('All Toys - Tukutoys')
     const [toys, setToys] = useState([]);
+    const [searchText, setSearchText] = useState("")
     useEffect(() => {
         fetch('http://localhost:5000/addToys')
             .then(res => res.json())
@@ -14,12 +15,20 @@ const AllToy = () => {
                 setToys(data)
             })
     }, [])
+
+    const handleSearchText = ()=>{
+        fetch(`http://localhost:5000/toySearch/${searchText}`)
+        .then(res => res.json())
+        .then(data =>{
+            setToys(data)
+        })
+    }
     return (
         <div className='bg-[#E8F7FF]'>
            <div>
             <div className='w-full ml-60'>
-                <input className='p-4 border-2 rounded-lg w-2/4 mt-12' placeholder='Search' type="text" name="search" id="" />
-                <button className='bg-gradient-to-r from-[#6D74E4] to-[#848FFF] py-5 rounded-e-md  px-4  text-xl text-white'> <ImSearch className='text-2xl'></ImSearch> </button>
+                <input onChange={(e)=> setSearchText(e.target.value)} className='p-4 border-2 rounded-lg w-2/4 mt-12' placeholder='Search' type="text" name="search" id="" />
+                <button onClick={handleSearchText} className='bg-gradient-to-r from-[#6D74E4] to-[#848FFF] py-5 rounded-e-md  px-4  text-xl text-white'> <ImSearch className='text-2xl'></ImSearch> </button>
             </div>
            </div>
             <h1 className='text-5xl text-center py-16'>All Toy</h1>
